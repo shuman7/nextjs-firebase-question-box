b96c68a19525be290205614beeda5364a14f14ce
shuman7 <shuman1231@gmail.com>
2022-12-10 03:31:15 +0900

    add infinite scroll

--- pages/questions/received.tsx
+++ pages/questions/received.tsx
@@ -1,60 +1,117 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { Question } from '../../models/Question'
 import Layout from '../../components/Layout'
 import { 
     collection,
+    DocumentData,
     getDocs,
     getFirestore,
     orderBy,
     query,
+    limit,
+    QuerySnapshot,
+    startAfter,
     where,
  } from 'firebase/firestore'
  import { useAuthentication } from '../../hooks/authentication'
  import dayjs from 'dayjs'
-
+import { create } from 'domain'
 
 export default function QuestionsReceived() {
     const [questions, setQuestions] = useState<Question[]>([])
     const { user } = useAuthentication()
+    const [isPaginationFinished, setIsPaginationFinished] = useState(false)
+    const scrollContainerRef = useRef(null)
 
-    useEffect(() => {
-        if (!process.browser) {
-            return
-        }
-        if (user === null) {
-            return
-        }
-    
-        async function loadQuestions(){
+        function createBaseQuery() {
             const db = getFirestore()
-            const q = query(
+            return query(
                 collection(db, 'questions'),
                 where('receiverUid', '==', user.uid),
-                orderBy('createdAt', 'desc')
+                orderBy('createdAt', 'desc'),
+                limit(10)
             )
-            const snapshot = await getDocs(q)
-    
-            if (snapshot.empty){
-                return
-            }
-    
+        }
+
+        function appendQuestions(snapshot: QuerySnapshot<DocumentData>){
             const gotQuestions = snapshot.docs.map((doc) => {
                 const question = doc.data() as Question
                 question.id = doc.id
                 return question
             })
-            setQuestions(gotQuestions)
+            setQuestions(questions.concat(gotQuestions))
+        }
+
+        async function loadQuestions(){
+            const snapshot = await getDocs(createBaseQuery())
+    
+            if (snapshot.empty){
+                setIsPaginationFinished(true)
+                return
+            }
+
+            appendQuestions(snapshot)
         }
     
-        loadQuestions()
-    }, [process.browser, user])
+        async function loadNextQuestions() {
+            if (questions.length === 0) {
+                return
+            }
+
+            const lastQuestion = questions[questions.length - 1]
+            const snapshot = await getDocs(
+                query(createBaseQuery(), startAfter(lastQuestion.createdAt))
+            )
+
+            if (snapshot.empty){
+                return
+            }
+
+            appendQuestions(snapshot)
+        }
+
+        useEffect(()=> {
+            if (!process.browser) {
+                return
+            }
+            if (user === null) {
+                return
+            }
+            loadQuestions()
+        }, [process.browser, user])
+
+        function onScroll(){
+            if (isPaginationFinished){
+                return
+            }
+            const container = scrollContainerRef.current
+            if (container === null) {
+                return
+            }
+    
+            const rect = container.getBoundingClientRect()
+            if (rect.top + rect.height > window.innerHeight) {
+                return
+            }
+
+            loadNextQuestions()
+        }
+
+
+        useEffect(() => {
+            window.addEventListener('scroll', onScroll)
+            return () => {
+                window.removeEventListener('scroll', onScroll)
+            }
+        }, [questions, scrollContainerRef.current, isPaginationFinished])
+
 
     return (
         <Layout>
             <h1 className="h4">受け取った質問一覧</h1>
 
             <div className="row justify-content-center">
-                <div className="col-12 col-md-6">
+                <div className="col-12 col-md-6" ref={scrollContainerRef}>
                     { questions.map((question) => (
                         <div className="card my-3" key={question.id}>
                             <div className="card-body">
